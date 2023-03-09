be8db98c858d0e58e252290a28340887fb6b7cef
katrine-mo-jensen <katrine.mo.jensen@gmail.com>
2023-03-09 14:35:34 +0100

    Lilo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,61 @@
-//
 let myPage = 1;
 const myAppElement = document.getElementById("myApp");
-// console.log(myAppElement);
 
+// entry point
 loadingScreen();
 setUpShowAllButton();
 setupSearchForm();
+fetchOneCharacter(5195);
 
-// Loading screen being called while we fetch data
+function fetchOneCharacter(myId) {
+  let URI = `https://api.disneyapi.dev/characters/${myId}`;
+
+  fetch(URI)
+    .then((response) => {
+      //console.log(response);
+
+      if (response.ok) {
+        return response.json();
+      } else {
+        alert("api error du får lige mickey mouse");
+        fetchOneCharacter(4703);
+      }
+    })
+    .then((data) => {
+      //console.log(data);
+      showCharacter(data);
+    })
+    .catch();
+}
+
+function showCharacter(myData) {
+  //  myAppElement
+  console.log(myData.name);
+
+  let myFilms = "<h3>Films:</h3>";
+  myData.films.map((film) => {
+    myFilms += `${film}, `;
+  });
+
+  let myTvShows = "<h3>TV Shows:</h3>";
+  myData.tvShows.map((show) => {
+    myTvShows += `${show}, `;
+  });
+
+  let myHTML = `<h2>${myData.name}</h2><img src="${myData.imageUrl}"><p>${myFilms}</p><p>${myTvShows}</p>`;
+  myAppElement.innerHTML = myHTML;
+}
+
+// loading screen  kaldes når vi henter data
 function loadingScreen() {
-  myAppElement.innerHTML = "<h2>Loading... </h2>";
+  myAppElement.innerHTML = "<h2>Loading...</h2>";
 }
 
 function setUpShowAllButton() {
   let showAllButton = document.getElementById("showAllButton");
   showAllButton.addEventListener("click", (e) => {
     myPage = 1;
-    fetchCharacterPage();
+    fetchAllCharacters();
   });
 }
 
@@ -25,20 +64,26 @@ function setupSearchForm() {
 
   searchButton.addEventListener("click", (e) => {
     e.preventDefault();
+
     let searchInput = document.getElementById("searchInput");
     let myValue = searchInput.value;
 
     if (myValue) {
-      console.log("vi har string " + myValue);
+      console.log(" vi har string " + myValue);
     } else {
-      alert("Indtast i søge felt");
+      alert("indtast i søge felt.");
     }
   });
 }
 
-/*^^^^^^Bo's code along^^^^^^*/
+function fetchCaracterPage() {
+  console.log("fetchCaracterPage");
+}
 
-const disneyAPI = "https://api.disneyapi.dev/characters?page=";
+/*↑↑↑↑↑↑↑ Bo's code-along ↑↑↑↑↑↑↑ */
+/*↓↓↓↓↓↓↓ Mit forsøg ↓↓↓↓↓↓↓*/
+
+/* const disneyAPI = "https://api.disneyapi.dev/characters?page=";
 
 let disneyFetch = fetch(disneyAPI)
   .then((response) => {
@@ -66,3 +111,4 @@ function setUpNavButtons() {}
 function showCharacterPage() {
   setUpNavButtons();
 }
+ */
